5912e5ad64121933b0808aa612f16cf7ff4f9626
My Name <my-name@chromium.org>
2020-11-13 09:06:53 +0100

    .

---
+++ TestWindowsService/TestWindowsService/Service1.cs
@@ -0,0 +1,42 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TestWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer timer1 = null;
+        
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1 = new Timer();
+            this.timer1.Interval = 10000;
+            this.timer1.Elapsed += new ElapsedEventHandler(this.timer1_Tick);
+        }
+
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            Libary.WriteErrorLog("Timer ticked and some job has been done succesfully");
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+            Libary.WriteErrorLog("Test window services stopped");
+        }
+    }
+}
